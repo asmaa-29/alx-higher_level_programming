68aaa9f30ef0e1c055a0b2d8e71bcef72ab232b2
asmaa <asmaammar29@gmail.com>
2024-01-04 08:13:28 -0800

    wh

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-import sys; print(sum(int(arg) for arg in sys.argv[1:]))
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv)
+    res = 0
+
+    for i in range(1, n):
+        res = res + int(sys.argv[i])
+    print(f"{res:d}")
