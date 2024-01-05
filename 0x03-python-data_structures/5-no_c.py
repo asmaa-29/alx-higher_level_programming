e46db5c10b3fd286a5825d0228a48ac000f97e17
asmaa <asmaammar29@gmail.com>
2024-01-05 14:23:40 -0800

    python-data_structures commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    chars = ""
-    for i in my_string:
-        if (i != 'c' and i != 'C'):
-            chars = chars + i
-    return chars
+    result = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        val = matrix[i]
-        for j in range(len(matrix[i])):
-            print("{:d}".format(val[j]), end=" " if len(val)-1 != j else "")
-        print("")
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
